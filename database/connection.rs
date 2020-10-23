5420a84fd1f633846ee761d2d897086b4d59f830
Eden Reich <eden.reich@gmail.com>
2020-10-23 22:14:25 +0200

    modify connection according to the docs

--- database/connection.rs
+++ database/connection.rs
@@ -3,6 +3,10 @@ use r2d2_diesel::ConnectionManager;
 
 type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 pub fn init_pool() -> Pool {
-    let manager = ConnectionManager::<MysqlConnection>::new("mysql://root:secret@127.0.0.1:3306/easyadmin");
-    Pool::new(manager).expect("db pool to be created")
+    let manager: ConnectionManager<MysqlConnection> = ConnectionManager::<MysqlConnection>::new(
+        std::env::var("DATABASE_URL").expect("DATABASE_URL to be set!"),
+    );
+    Pool::builder()
+        .build(manager)
+        .expect("failed to create pool")
 }
