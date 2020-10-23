6f5c46bcd8c2c03d603963762c191050e98f048e
Eden Reich <eden.reich@gmail.com>
2020-10-23 02:29:54 +0200

    use 127.0.0.1 instead of localhost
    later will extract this as env variable but for now just hard-code it

--- database/connection.rs
+++ database/connection.rs
@@ -3,6 +3,6 @@ use r2d2_diesel::ConnectionManager;
 
 type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 pub fn init_pool() -> Pool {
-    let manager = ConnectionManager::<MysqlConnection>::new("mysql://root:secret@localhost:3306/easyadmin");
+    let manager = ConnectionManager::<MysqlConnection>::new("mysql://root:secret@127.0.0.1:3306/easyadmin");
     Pool::new(manager).expect("db pool to be created")
 }
